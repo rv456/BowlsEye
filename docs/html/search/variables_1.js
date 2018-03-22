305587c36ac8c39bc43ead76fbb210b664b6887a
Harvey Leicester <harveyleicester1@gmail.com>
2018-03-22 13:17:38 +0000

    added some doxygen stuff

---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pinb1',['pinB1',['../structStepperMotor__t.html#a36991ba998b015a0844d669fb433002d',1,'StepperMotor_t']]],
+  ['pinena',['pinEnA',['../structStepperMotor__t.html#a4f7b7dab294af45332155539d06070c5',1,'StepperMotor_t']]]
+];
