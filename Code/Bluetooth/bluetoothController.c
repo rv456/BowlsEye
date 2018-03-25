26c7c0401789d0590879e819cb735893df01683d
Harvey Leicester <harveyleicester1@gmail.com>
2018-03-25 16:02:57 +0100

    updated readme

--- Code/Bluetooth/bluetoothController.c
+++ Code/Bluetooth/bluetoothController.c
@@ -1,12 +1,23 @@
+/*!
+ * @file bluetoothController.c
+ *
+ * 
+ * @author Harvey Leicester
+ */
+
 #include "bluetoothController.h"
 
 char PC_ADDRESS_[18] = "34:E6:AD:06:21:46";
 
+/*!
+ * Constructor for BluetoothServer_t
+ * @param server			Instance of BluetoothServer_t
+ */
 void BluetoothServer_construct(BluetoothServer_t *server){
 
 	server->opt = sizeof(server->remAddr);
 	server->sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-	
+
 	server->locAddr.rc_family = AF_BLUETOOTH;
 	server->locAddr.rc_bdaddr = *BDADDR_ANY;
 	server->locAddr.rc_channel = (uint8_t)1;
@@ -21,6 +32,10 @@ void BluetoothServer_construct(BluetoothServer_t *server){
 	BluetoothServer_enable(server);
 }
 
+/*!
+ * Enablea the Bluetooth server
+ * @param server			Instance of BluetoothServer_t
+ */
 void BluetoothServer_enable(BluetoothServer_t *server){
 
 	listen(server->sock, 1);
@@ -31,12 +46,21 @@ void BluetoothServer_enable(BluetoothServer_t *server){
 	memset(server->buf, 0, sizeof(server->buf));
 }
 
+/*!
+ * Disables Bluetooth server
+ * @param server			Instance of BluetoothServer_t
+ */
 void BluetoothServer_disable(BluetoothServer_t *server){
 
 	close(server->client);
 	close(server->sock);
 }
 
+/*!
+ * Read integer value(s) from a connected Bluetooth client
+ * @param server			Instance of BluetoothServer_t
+ * @param dst				Destination of received data
+ */
 void BluetoothServer_readInt(BluetoothServer_t *server, int *dst){
 
 	int bytesRead = read(server->client, server->recData, sizeof(server->recData));
@@ -52,23 +76,32 @@ void BluetoothServer_readInt(BluetoothServer_t *server, int *dst){
 	}
 }
 
+/*!
+ * Read character value(s) from a connected Bluetooth client
+ * @param server			Instance of BluetoothServer_t
+ * @param dst				Destination of received data
+ */
 void BluetoothServer_readChar(BluetoothServer_t *server, char *dst){
 
 	int bytesRead = read(server->client, server->recData, sizeof(server->recData));
 	if(bytesRead > 0){
 
-		printf("received [%s]", (server->recData));	
+		printf("received [%s]", (server->recData));
 
 		printf("\n");
 		*dst = (char)(server->recData[0]);
 	}
 }
 
+/*!
+ * constructor for BluetoothClient_t
+ * @param client			Instance of BluetoothClient_t
+ */
 void BluetoothClient_construct(BluetoothClient_t *client){
 
 	client->dst = PC_ADDRESS_;
 	client->sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-	
+
 	client->addr.rc_family = AF_BLUETOOTH;
 	client->addr.rc_channel = (uint8_t)1;
 	str2ba(client->dst, &(client->addr.rc_bdaddr));
@@ -76,23 +109,41 @@ void BluetoothClient_construct(BluetoothClient_t *client){
 	BluetoothClient_enable(client);
 }
 
+/*!
+ * Enables a Bluetooth client
+ * @param client			Instance of BluetoothClient_t
+ */
 void BluetoothClient_enable(BluetoothClient_t *client){
 
 	client->status = connect(client->sock, (struct sockaddr *)&(client->addr), sizeof(client->addr));
 	BluetoothClient_checkError(client);
 }
 
+/*!
+ * Disables a Bluetooth client
+ * @param client			Instance of BluetoothClient_t
+ */
 void BluetoothClient_disable(BluetoothClient_t *client){
 
 	close(client->sock);
 }
 
+/*!
+ * Send a string of data to connected Bluetooth server
+ * @param client			Instance of bluetoothClient_t
+ * @param data				Data to send
+ * @param size				Size of data to send
+ */
 void BluetoothClient_sendString(BluetoothClient_t *client, char *data, int size){
 
 	client->status = write(client->sock, data, size);
 	BluetoothClient_checkError(client);
 }
 
+/*!
+ * Check for errors in status of Bluetooth client
+ * @param client			Instance of BluetoothClient_t
+ */
 void BluetoothClient_checkError(BluetoothClient_t *client){
 
 	if(client->status < 0){
@@ -105,5 +156,6 @@ void BluetoothClient_checkError(BluetoothClient_t *client){
 
 
 
- 
+
+
 
